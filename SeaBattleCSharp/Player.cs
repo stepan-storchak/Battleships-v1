56b63d870e53a1f604b2556128ab76646c78689b
Stepan_Storchak <deptul2@list.ru>
2025-11-16 16:37:31 +0700

    Основные игровые классы (GameBoard, Ship, Player)

---
+++ SeaBattleCSharp/Player.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SeaBattleCSharp
+{
+    public abstract class Player
+    {
+        protected string name;
+        protected GameBoard myBoard;
+        protected GameBoard enemyBoard;
+        protected List<Ship> ships;
+
+        public Player(string name)
+        {
+            this.name = name;
+            myBoard = new GameBoard();
+            enemyBoard = new GameBoard();
+            ships = new List<Ship>();
+        }
+
+        public abstract void PlaceShips();
+        public abstract void MakeMove(Player enemy);
+
+        public bool AllShipsSunk()
+        {
+            return ships.All(ship => ship.IsSunk());
+        }
+
+        public ShotResult GetShotResult(Coordinate coord)
+        {
+            ShotResult result = myBoard.ReceiveShot(coord);
+
+            if (result == ShotResult.Hit)
+            {
+                foreach (var ship in ships)
+                {
+                    if (ship.Coordinates.Any(c => c.X == coord.X && c.Y == coord.Y))
+                    {
+                        bool wasHit = ship.TakeHit(coord);
+                        if (wasHit && ship.IsSunk())
+                        {
+                            result = ShotResult.Sunk;
+                        }
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public void AddShip(Ship ship)
+        {
+            ships.Add(ship);
+        }
+
+        public string GetName() => name;
+        public GameBoard GetMyBoard() => myBoard;
+        public GameBoard GetEnemyBoard() => enemyBoard;
+    }
+}
---
+++ SeaBattleCSharp/Ship.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SeaBattleCSharp
+{
+    public class Ship
+    {
+        public int Size { get; }
+        public Coordinate Start { get; }
+        public Orientation Orientation { get; }
+        public List<Coordinate> Coordinates { get; }
+        public List<Coordinate> Hits { get; } = new List<Coordinate>();
+
+        public Ship(int size, Coordinate start, Orientation orientation)
+        {
+            Size = size;
+            Start = start;
+            Orientation = orientation;
+            Coordinates = CalculateCoordinates();
+        }
+
+        private List<Coordinate> CalculateCoordinates()
+        {
+            var coords = new List<Coordinate>();
+            for (int i = 0; i < Size; i++)
+            {
+                if (Orientation == Orientation.Horizontal)
+                {
+                    coords.Add(new Coordinate(Start.X + i, Start.Y));
+                }
+                else
+                {
+                    coords.Add(new Coordinate(Start.X, Start.Y + i));
+                }
+            }
+            return coords;
+        }
+
+        public bool TakeHit(Coordinate coord)
+        {
+            if (Coordinates.Any(c => c.X == coord.X && c.Y == coord.Y) &&
+                !Hits.Any(c => c.X == coord.X && c.Y == coord.Y))
+            {
+                Hits.Add(coord);
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsSunk()
+        {
+            return Hits.Count == Size;
+        }
+    }
+}
